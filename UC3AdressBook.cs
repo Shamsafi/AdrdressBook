3b52088887063b46d78abd7afdb606bc5344ac48
Safi <mdsafi729@gmail.com>
2023-05-07 16:36:18 +0530

    [shamsuddin]Fix: Adding UC3 file

---
+++ UC3AdressBook.cs
@@ -0,0 +1,63 @@
+﻿using AddressBook;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        private List<Contact> contacts;
+
+        public AddressBook()
+        {
+            contacts = new List<Contact>();
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void EditContact(string firstName, string lastName)
+        {
+            Contact contact = FindContact(firstName, lastName);
+            if (contact != null)
+            {
+                Console.WriteLine("Enter new address:");
+                contact.Address = Console.ReadLine();
+                Console.WriteLine("Enter new city:");
+                contact.City = Console.ReadLine();
+                Console.WriteLine("Enter new state:");
+                contact.State = Console.ReadLine();
+                Console.WriteLine("Enter new zip:");
+                contact.Zip = Console.ReadLine();
+                Console.WriteLine("Enter new phone number:");
+                contact.PhoneNumber = Console.ReadLine();
+                Console.WriteLine("Enter new email:");
+                contact.Email = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Contact not found.");
+            }
+        }
+
+        public void DisplayContacts()
+        {
+            foreach (Contact contact in contacts)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+
+        private Contact FindContact(string firstName, string lastName)
+        {
+            foreach (Contact contact in contacts)
+            {
+                if (contact.FirstName == firstName && contact.LastName == lastName)
+                {
+                    return contact;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ UC3Main.cs
@@ -0,0 +1,48 @@
+﻿using AddressBook;
+
+namespace AddressBook
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+
+            Console.WriteLine("Add a new contact:");
+            Console.Write("First name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Address: ");
+            string address = Console.ReadLine();
+            Console.Write("City: ");
+            string city = Console.ReadLine();
+            Console.Write("State: ");
+            string state = Console.ReadLine();
+            Console.Write("Zip: ");
+            string zip = Console.ReadLine();
+            Console.Write("Phone number: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            addressBook.AddContact(newContact);
+
+            Console.WriteLine("All contacts:");
+            addressBook.DisplayContacts();
+
+            Console.WriteLine("Edit an existing contact:");
+            Console.Write("Enter first name of contact to edit: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter last name of contact to edit: ");
+            lastName = Console.ReadLine();
+
+            addressBook.EditContact(firstName, lastName);
+
+            Console.WriteLine("All contacts:");
+            addressBook.DisplayContacts();
+        }
+    }
+
+}
