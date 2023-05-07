670d58afc7793662f3a2c984ad909991ec851c0f
Safi <mdsafi729@gmail.com>
2023-05-07 16:44:44 +0530

    [shamsuddin]Fix: Adding UC8 file

---
+++ UC8.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Person(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}\nAddress: {Address}\nCity: {City}\nState: {State}\nZip: {Zip}\nPhone Number: {PhoneNumber}\nEmail: {Email}";
+        }
+    }
+
+    class AddressBook
+    {
+        private List<Person> people;
+
+        public AddressBook()
+        {
+            people = new List<Person>();
+        }
+
+        public void AddPerson(Person person)
+        {
+            people.Add(person);
+        }
+
+        public List<Person> SearchByCity(string city)
+        {
+            return people.Where(person => person.City == city).ToList();
+        }
+
+        public List<Person> SearchByState(string state)
+        {
+            return people.Where(person => person.State == state).ToList();
+        }
+    }
+}
---
+++ UC8Main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook1 = new AddressBook();
+            addressBook1.AddPerson(new Person("Sam", "Smith", "WhiteField", "Banglore", "KA", "585417", "9857412630", "smith@gmail.com"));
+            addressBook1.AddPerson(new Person("Will", "Smith", "Ameerpet", "Hyderabad", "TL", "545214", "8745213698", "will@gmail.com"));
+
+            AddressBook addressBook2 = new AddressBook();
+            addressBook2.AddPerson(new Person("Bob", "Curran", "Msk", "Kerala", "KL", "54321", "874512966", "bob@gmail.com"));
+            addressBook2.AddPerson(new Person("Alice", "Jones", "MG-Road", "Bijapur", "KA", "09876", "845129637", "alice@gmail.com.com"));
+
+            List<AddressBook> addressBooks = new List<AddressBook> { addressBook1, addressBook2 };
+
+            Console.WriteLine("Search for a person by city or state:");
+            Console.Write("City or state: ");
+            string cityOrState = Console.ReadLine();
+
+            List<Person> searchResults = new List<Person>();
+            foreach (AddressBook addressBook in addressBooks)
+            {
+                searchResults.AddRange(addressBook.SearchByCity(cityOrState));
+                searchResults.AddRange(addressBook.SearchByState(cityOrState));
+            }
+
+            if (searchResults.Count == 0)
+            {
+                Console.WriteLine("No results found.");
+            }
+            else
+            {
+                Console.WriteLine($"Found {searchResults.Count} result(s):");
+                foreach (Person person in searchResults)
+                {
+                    Console.WriteLine(person);
+                }
+            }
+        }
+    }
+}
