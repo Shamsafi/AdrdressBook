be997ac5ababcf50a1a4178525d544b0a7b06bb4
Safi <mdsafi729@gmail.com>
2023-05-07 16:38:06 +0530

    [shamsuddin]Fix: Adding UC4 file

---
+++ UC4AddressBook.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        private List<Contact> contacts;
+
+        public AddressBook()
+        {
+            contacts = new List<Contact>();
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void DeleteContact(string firstName, string lastName)
+        {
+            Contact contact = FindContact(firstName, lastName);
+            if (contact != null)
+            {
+                contacts.Remove(contact);
+                Console.WriteLine("Contact deleted.");
+            }
+            else
+            {
+                Console.WriteLine("Contact not found.");
+            }
+        }
+
+        public void DisplayContacts()
+        {
+            foreach (Contact contact in contacts)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+
+        private Contact FindContact(string firstName, string lastName)
+        {
+            foreach (Contact contact in contacts)
+            {
+                if (contact.FirstName == firstName && contact.LastName == lastName)
+                {
+                    return contact;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ UC4Main.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+
+            Console.WriteLine("Add a new contact:");
+            Console.Write("First name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Address: ");
+            string address = Console.ReadLine();
+            Console.Write("City: ");
+            string city = Console.ReadLine();
+            Console.Write("State: ");
+            string state = Console.ReadLine();
+            Console.Write("Zip: ");
+            string zip = Console.ReadLine();
+            Console.Write("Phone number: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            addressBook.AddContact(newContact);
+
+            Console.WriteLine("All contacts:");
+            addressBook.DisplayContacts();
+
+            Console.WriteLine("Delete an existing contact:");
+            Console.Write("Enter first name of contact to delete: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter last name of contact to delete: ");
+            lastName = Console.ReadLine();
+
+            addressBook.DeleteContact(firstName, lastName);
+
+            Console.WriteLine("All contacts:");
+            addressBook.DisplayContacts();
+        }
+    }
+}
